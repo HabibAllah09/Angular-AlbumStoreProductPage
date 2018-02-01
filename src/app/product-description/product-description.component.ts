090194725b5b864c18b6b2824c02b37141be7fa1
HabibAllah <habib891allah@gmail.com>
2018-02-01 16:13:26 +0200

    "Add Provider Service and Change Hard Coded Data To JSON We Get from Http Request"

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  // #4: to be able to use a class property we need ...
+  // ... to define a one in the class
+  albumInfo;
+
+  // #1: in type script: the constructor arguments will be ...
+  // ... automatically added to the 'this' object.
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    // #2: when the response from the server arrives
+    // the callback passed to the subscribe
+    // is called by the observable.
+    this._productService.getAlbum(1).subscribe(
+      // #3: the response now is stored in the class properties
+      response => this.albumInfo = response
+    );
   }
 
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+// to map the http output as json
+// and to use the map function
+// we have to import it from rxjs
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+  // private _albumUrl: string = '../assets/album.json';
+  // there's no need to declair the type of the class property
+  private _albumUrl = '../assets/album.json';
+
+  getAlbum(id: number) {
+    // the get returns a response
+    // the response is not a json
+    // the map function maps the response into json
+    // the .json function turn the response into json
+    return this._http.get(this._albumUrl).map(
+      (response) => response.json()
+    );
+  }
+
+}

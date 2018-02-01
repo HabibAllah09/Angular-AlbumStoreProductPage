a14359027cd8f0dd8dc09f9999ccbd607ec8f318
HabibAllah <habib891allah@gmail.com>
2018-02-01 16:17:30 +0200

    "Relocate getAlbum docs"

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,11 +15,11 @@ export class ProductService {
   // there's no need to declair the type of the class property
   private _albumUrl = '../assets/album.json';
 
+  // the get returns a response
+  // the response is not a json
+  // the map function maps the response into json
+  // the .json function turn the response into json
   getAlbum(id: number) {
-    // the get returns a response
-    // the response is not a json
-    // the map function maps the response into json
-    // the .json function turn the response into json
     return this._http.get(this._albumUrl).map(
       (response) => response.json()
     );
